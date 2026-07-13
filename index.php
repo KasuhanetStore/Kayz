37acd43544bdb535462971a1601658fd82c1154a
KasuhanetStore <zukarnain94@gmail.com>
2026-07-13 13:00:53 +0800

    Add initial HTML structure with Google Analytics

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ms">
+<head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-QNQZT77GSR"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-QNQZT77GSR');
+    </script>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kasuhanet Store</title>
+</head>
+<body>
+    <?php echo "<h1>Website Kasuhanet</h1>"; ?>
+</body>
+</html>
